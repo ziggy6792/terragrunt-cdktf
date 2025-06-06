935b41af0b2f23563846578be148fa3b503e6c4a
Simon <simonaverhoeven@gmail.com>
2025-06-06 11:29:33 +0800

    add context

--- cdktf/stacks/param-store/ParamStoreStack.ts
+++ cdktf/stacks/param-store/ParamStoreStack.ts
@@ -11,6 +11,8 @@ export class ParamStoreStack extends AwsBaseStack {
   constructor(scope: Construct, id: string, props: ParamStoreStackProps) {
     super(scope, id, props);
 
+    // const dynamoStack = this.context.getStack(DynamoStack);
+
     const paramStore = new AwsSsmParameter(this, 'param-store', {
       name: `/my-app-2/${props.stageName}/dynamo-table-id`,
       value: props.dynamoTable.dynamodbTableIdOutput,
